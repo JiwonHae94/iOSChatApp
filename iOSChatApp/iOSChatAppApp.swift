d3c8fec5c0d732f2e26724a760e210c2596552fa
JiwonHae <hjiwon1994@gmail.com>
2021-12-28 00:03:32 +0900

    Initial Commit

---
+++ iOSChatApp/iOSChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSChatAppApp.swift
+//  iOSChatApp
+//
+//  Created by Jiwon_Hae on 2021/12/27.
+//
+
+import SwiftUI
+
+@main
+struct iOSChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
